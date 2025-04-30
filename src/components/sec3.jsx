4bbd79289ee1d839a8fd56409b77790ff2e75c84
22051960 <pratyushchauhan147code@gmail.com>
2025-04-30 18:45:13 +0530

    Navigate

--- src/components/sec3.jsx
+++ src/components/sec3.jsx
@@ -14,13 +14,13 @@ const Sec3 = () => {
             </div>
             <div data-scroll  data-scroll-speed="0" className="top flex justify-center ">
                 <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[20%] rounded-2xl h-[20vw] m-2  bg-white'></motion.div>
-                <motion.a href='/ps' whileHover={{scale:1.05,backgroundColor:"#a867e6"}}  initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[40%] rounded-2xl  h-[20vw] m-2  flex justify-center  items-center bg-white'> <h1 className=' font-[anton] text-[5vw] text-black'>SKILLS</h1></motion.a>
+                <motion.div  onClick={()=>{navigate('/ps')}} whileHover={{scale:1.05,backgroundColor:"#a867e6"}}  initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[40%] rounded-2xl  h-[20vw] m-2  flex justify-center  items-center bg-white'> <h1 className=' font-[anton] text-[5vw] text-black'>SKILLS</h1></motion.div>
                 <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[20%] rounded-2xl  h-[20vw] m-2 bg-red-400'></motion.div>
             </div>
 
             <div data-scroll  data-scroll-speed="0" className="top flex justify-center ">
                 <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[10%] rounded-2xl h-[20vw] m-2  bg-blue-400'></motion.div>
-                <motion.a href='/ps' initial={{scale:0,opacity:0}}  whileHover={{scale:1.05,backgroundColor:"#63ff92"}}   whileInView={{scale:1 , opacity:1}} className='w-[50%] rounded-2xl  h-[20vw] m-2  flex justify-center  items-center bg-white'> <h1 className=' font-[anton] text-[5vw] text-black'>PROBLEM SOLVING</h1></motion.a>
+                <motion.div  onClick={()=>{navigate('/ps')}} initial={{scale:0,opacity:0}}  whileHover={{scale:1.05,backgroundColor:"#63ff92"}}   whileInView={{scale:1 , opacity:1}} className='w-[50%] rounded-2xl  h-[20vw] m-2  flex justify-center  items-center bg-white'> <h1 className=' font-[anton] text-[5vw] text-black'>PROBLEM SOLVING</h1></motion.div>
                 <motion.div initial={{scale:0,opacity:0}} whileInView={{scale:1 , opacity:1}} className='w-[20%] rounded-2xl  h-[20vw] m-2 bg-white'></motion.div>
             </div>
             
